fb611b021f7cfd56c8962a916e147711953e522f
TaherJerbi <jerbi.taherr@gmail.com>
2022-01-27 14:56:11 +0100

    responsive sidebar

--- src/components/LinkCard/LinkCard.jsx
+++ src/components/LinkCard/LinkCard.jsx
@@ -1,14 +1,15 @@
 import { Button, HStack, Text, Icon } from "@chakra-ui/react"
 import  NextLink from "next/link"
 
-function LinkCard({text, url, icon : LinkIcon, selected}){
+function LinkCard({text, url, icon : LinkIcon, selected, showText}){
   return (
     <NextLink href={url}>
-      <Button w="full" bgColor={!selected && "inherit"} color="gray.500" borderRadius="1" justifyContent="flex-start">
-        <HStack spacing={2}>
+      <Button w="full" bgColor={!selected && "inherit"} color="gray.500" borderRadius="1" justifyContent={showText ? "flex-start" : 'center'}>
+        {showText && <HStack spacing={2}>
           {LinkIcon && <Icon data-testid={`link-card-icon_${text}_${url}`} as={LinkIcon}/> }
           <Text>{text}</Text>
-        </HStack>
+        </HStack>}
+        {!showText && <Icon data-testid={`link-card-icon_${text}_${url}`} as={LinkIcon}/>}
       </Button>
     </NextLink>
   )
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,23 +1,28 @@
-import { Heading, HStack, Icon, Link, VStack } from "@chakra-ui/react"
+import { Heading, HStack, Icon, Link, VStack, useBreakpointValue } from "@chakra-ui/react"
 import NextLink from "next/link"
 import { FaTwitter } from 'react-icons/fa';
 import LinkCard from "../LinkCard"
 
 function SideBar({ topLinks, bottomLinks }){
+  const width = useBreakpointValue({base: '50px', md:'25%'})
+  const showText = useBreakpointValue({base: false, md:true})
+  const alignItems = useBreakpointValue({base:'center', md:'flex-start'})
   return (
-    <VStack h="full" w="full" direction="column" justifyContent="space-between" alignItems="center" py={5} px={5}>
-      <VStack w="full" alignItems='flex-start'>
-        {topLinks?.map(link => <LinkCard {...link}/>)}
+    <VStack h="100vh" w={width} direction="column" bgColor="gray.50" justifyContent="space-between" alignItems="center" py={5} px={5}>
+      <VStack w="full" alignItems={alignItems}>
+        {topLinks?.map(link => <LinkCard {...link} showText={showText}/>)}
       </VStack>
-      <VStack w="full" alignItems='flex-start'>
-        <VStack  alignItems='flex-start'>
-          {bottomLinks?.map(link => <Link key={`link${link.url}${link.text}`} href={link.url} as={NextLink}>{link.text}</Link>)}
-        </VStack>
+      <VStack w="full" alignItems={alignItems}>
+        {showText && 
+          <VStack  alignItems='flex-start'>
+            {bottomLinks?.map(link => <Link key={`link${link.url}${link.text}`} href={link.url} as={NextLink}>{link.text}</Link>)}
+          </VStack>}
 
-        <HStack pt={4} justifyContent="space-between" w="full" alignItems="center">
+        {showText && <HStack pt={4} justifyContent="space-between" w="full" alignItems="center">
           <Heading size="lg" color="gray.600">Curated</Heading>
           <Icon as={FaTwitter} w={5} h={5} color="gray.300"/>
-        </HStack>
+        </HStack>}
+        {!showText && <Icon as={FaTwitter} w={5} h={5} color="gray.300"/>}
       </VStack>
       
     </VStack>
